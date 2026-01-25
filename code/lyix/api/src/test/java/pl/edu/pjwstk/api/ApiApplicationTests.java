0fecad05a4562b3a55b3db7de8732c1de0f046be
janiszvanish <janisia66@gmail.com>
2026-01-25 20:48:11 +0100

    B: Project modules configuration

---
+++ code/lyix/api/src/test/java/pl/edu/pjwstk/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.edu.pjwstk.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
